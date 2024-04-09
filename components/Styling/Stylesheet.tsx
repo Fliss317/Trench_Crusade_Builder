b4b1df7fa3fbb03db31baa5816fd38291754371b
Felicity Cullen <felicity.cullen@mail.com>
2024-04-09 02:21:48 +0100

    Styling initial component file

---
+++ components/Styling/Stylesheet.tsx
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      borderWidth: 10,
+      backgroundColor: '#255',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    dataTable: {
+        alignItems: 'stretch',
+    },
+    tableHeader: {
+      backgroundColor: 'green', 
+    },
+    tableRow: {
+        backgroundColor: '#fff',
+    }
+  });
+  
